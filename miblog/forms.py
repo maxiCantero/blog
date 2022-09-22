95e339388f1edcfa10cb17c7aab32f571930011c
mcantero <canterorodriguezm@outlook.com>
2022-09-22 01:34:16 -0300

    Primer commit de blog

---
+++ miblog/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+class FormularioPost(forms.Form):
+    titulo = forms.CharField()
+    contenido = forms.CharField()
+    fecha_creacion = forms.DateField()
+    # categoria = forms.ForeignKey(Categoria, on_delete=forms.CASCADE)
+    # autor = forms.ForeignKey(Autor, on_delete=forms.CASCADE)
+
+class FormularioCategoria(forms.Form):
+    titulo = forms.CharField()
+
+class FormularioAutor(forms.Form):
+    nombre = forms.CharField()
+    correo = forms.EmailField()
+
+
+
